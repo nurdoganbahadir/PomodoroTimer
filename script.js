f06c83213bf52b20b9db761139a5cd838c09fd9e
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-07-22 17:49:01 +0300

    alarm added and update

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ const startBtn = document.querySelector(".start");
 const stopBtn = document.querySelector(".stop");
 const resetBtn = document.querySelector(".reset");
 const display = document.querySelector("h2");
+const alarmSound = document.querySelector("audio");
 
-const minute = 25;
+const minute = 1;
 const seconds = 60;
 
 let totalTime = minute * seconds;
+let isAlarmPlayed = false;
 
 function timeRemaining() {
   const minutes = Math.floor(totalTime / 60);
@@ -15,9 +17,11 @@ function timeRemaining() {
   display.textContent = `${minutes}:${seconds}`;
 
   //zaman dolduysa
-  if (totalTime <= 0) {
+  if (totalTime <= 0 && !isAlarmPlayed) {
     clearInterval(interval);
     display.textContent = "Zaman Doldu!";
+    alarmSound.play();
+    isAlarmPlayed = true;
   }
 }
 
